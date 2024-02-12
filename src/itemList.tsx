e6d0f218dc93807eae024078c8820b86b04399fb
siggo <sig.goldbach@gmail.com>
2024-02-12 15:52:44 +0100

    Changed ItemINFO

--- src/itemList.tsx
+++ src/itemList.tsx
@@ -2,14 +2,31 @@ import React from "react";
 
 
 
-function DisplayItem(item: Item){
+function DisplayItem(item: CompleteItem){
+    return(
+        <>
+        <div>
+    
+        </div>
+        </>
+
+    )
+   
 
 }
 
 
 
-function ItemList(items:BasketItem[]){
-    items.map((item)=>DisplayItem)
+function ItemList(items: Item[]){
+    return(
+        <><div>
+
+
+        </div>
+
+        </>
+    )
+    
 
 
 
@@ -20,20 +37,22 @@ function ItemList(items:BasketItem[]){
 
 
 interface CompleteItem{
+    itemInfor: ItemInfo;
     item: Item;
-    basketItem: BasketItem;
 }
 
-interface Item {
+interface ItemInfo {
     id: String;
     name: String;
+    description: String;
+    weight: number;
     price: number;
     currency: String;
     rebateQuantity: number;
     rebatePercent: number;
     upsellProductId: string;
 }
-interface BasketItem{
+interface Item{
     id: String;
     quantity: number;
     giftWrap: Boolean;
