b881997dcdcc246e7dd1f55f5a8f0cf577b8c17b
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-02-19 13:45:20 +0100

    Commiting to mergre

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './ShoppingCart.tsx'
+import ShoppingCart from './ShoppingCart.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <ShoppingCart/>
     </>
   )
 }
