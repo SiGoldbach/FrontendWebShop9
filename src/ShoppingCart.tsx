480d7bfbdfef07e8e128ae5ee626b3deb0ee6a83
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-02-19 20:34:26 +0100

    Now the ShoppingCart works as it should with const not being a state and increase/decrease quantity working properly

--- src/ShoppingCart.tsx
+++ src/ShoppingCart.tsx
@@ -3,14 +3,77 @@ import './Data/product.json'
 import './index.css'
 import DisplayItem from "./displayItem.jsx";
 
+function PutItemsInBasket(){
+    const items: Item[] = []
+    const item1: Item = {
+        id: "vitamin-d-90-100",
+        quantity: 2,
+        giftWrap: true
+    }
+    const item2: Item = {
+        id: "vitamin-c-500-250",
+        quantity: 1,
+        giftWrap: false
+    }
+    const item3: Item = {
+        id: "vitamin-c-depot-500-250",
+        quantity: 7,
+        giftWrap: false
+    }
+    items.push(item1, item2, item3)
+    const itemInfos: ItemInfo[] = []
+    const iteminfo1:ItemInfo= {
+        id: "vitamin-d-90-100",
+        name: "D-vitamin",
+        price: 116,
+        currency: "DKK",
+        rebateQuantity: 3,
+        rebatePercent: 10,
+        upsellProductId: "null"
+    }
+    const iteminfo2:ItemInfo= {
+        id: "vitamin-c-500-250",
+        name: "C-vitamin",
+        price: 150,
+        currency: "DKK",
+        rebateQuantity: 2,
+        rebatePercent: 25,
+        upsellProductId: "vitamin-c-depot-500-250"
+    }
+    const iteminfo3:ItemInfo= {
+        id: "vitamin-c-depot-500-250",
+        name: "C-vitamin Depot",
+        price: 175,
+        currency: "DKK",
+        rebateQuantity: 3,
+        rebatePercent: 10,
+        upsellProductId: "null"
+    }
+    itemInfos.push(iteminfo1,iteminfo2,iteminfo3);
+    const completeItems: CompleteItem[] =[]
+    for(let i=0;i<items.length;i++){
+        const completeitem :CompleteItem={
+            item:items[i],
+            itemInfo:itemInfos[i]
+            
+        }
+        completeItems.push(completeitem)
+
+    }
+    console.log("Items are getting returned")
+    return completeItems
+
+
+
+}
+
 
 
 function ShoppingCart() {
     //Theese two consts define the state of this component
-    const [TotalPrice, setTotalPrice] = useState(0)
-    const [completeItem, setCompleteItems] = useState(Array<CompleteItem>)
+    const [completeItems, setCompleteItems] = useState(PutItemsInBasket())
     //This method is used to call display item once for each item in the shopping cart ##STYLE HERE## it has to be <li> component
-    const itemsToDisplay = completeItem.map(CompleteItemInList =>
+    const itemsToDisplay = completeItems.map(CompleteItemInList =>
         <li key={CompleteItemInList.item.id}>
 
             <DisplayItem
@@ -28,61 +91,12 @@ function ShoppingCart() {
         </li>
     )
     //Function for calculating the total price of the shopping cart 
-    function calculateTotalPrice() {
-        let price: number = 0;
-        console.log("Calculating total price")
-        for (let i = 0; i < completeItem.length; i++) {
-            price = price + completeItem[i].itemInfo.price * completeItem[i].item.quantity;
-        }
-        setTotalPrice(price)
-    }
+
     //Making the shopping cart 
-    const currentItemList: Item[] = []
-    const item1: Item = {
-        id: "vitamin-d-90-100",
-        quantity: 2,
-        giftWrap: true
-    }
-    const item2: Item = {
-        id: "vitamin-c-500-250",
-        quantity: 1,
-        giftWrap: false
-    }
-    const item3: Item = {
-        id: "vitamin-c-depot-500-250",
-        quantity: 7,
-        giftWrap: false
-    }
-    currentItemList.push(item1, item2, item3)
+
     //console.log(currentItemList)
     // This function is a temprorary solution when the backend is up and running the https request will go there. 
-    async function fecthCompleteItems() {
-        //console.log("Trying to fetch items")
-        const response = await fetch('https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json')
-        const returnedItems = await response.text()
-        const parsedItems: ItemInfo[] = JSON.parse(returnedItems)
-        const chosenItems: CompleteItem[] = []
-        //console.log(parsedItems.length)
-        //console.log(currentItemList.length)
-        for (let i = 0; i < parsedItems.length; i++) {
-            for (let j = 0; j < currentItemList.length; j++) {
-                //console.log("in loop iteration: ", j)
-                if (parsedItems[i].id === currentItemList[j].id) {
-                    const completeItem: CompleteItem = {
-                        itemInfo: parsedItems[i],
-                        item: currentItemList[j]
-                    }
-                    chosenItems.push(completeItem)
 
-                }
-            }
-        }
-        setCompleteItems(chosenItems)
-        //console.log("the chosen items are: ", chosenItems)
-        //console.log(CompleteItem)
-        console.log("The items in complete items are: ", completeItem)
-        console.log(completeItem[0].item.id, " should be the same ", completeItem[0].itemInfo.id)
-    }
     /**
      * Theese next four function are passed along to display item,
      * with the purpose of chaning state of the shopping basket.
@@ -91,18 +105,17 @@ function ShoppingCart() {
     function removeItem(id: string) {
 
         // works by creating a new set of items and filtering out the one with matching id
-        const tempCurrentItems: CompleteItem[] = completeItem.filter(item => item.item.id !== id);
+        const tempCurrentItems: CompleteItem[] = completeItems.filter(item => item.item.id !== id);
         
         setCompleteItems(tempCurrentItems)
-        calculateTotalPrice()
     }
     //Decrease the amount of a certain item by one 
 
     function decreaseQuantity(id: string) {
-        const currentItems: CompleteItem[] = completeItem;
+        const currentItems: CompleteItem[] = [...completeItems];
         for (let i = 0; i < currentItems.length; i++) {
-            if (completeItem[i].item.id === id) {
-                if (completeItem[i].item.quantity === 1) {
+            if (completeItems[i].item.id === id) {
+                if (completeItems[i].item.quantity === 1) {
                     break;
                 }
                 currentItems[i].item.quantity--;
@@ -112,71 +125,65 @@ function ShoppingCart() {
 
         }
         setCompleteItems(currentItems)
-        calculateTotalPrice()
 
     }
     //Increase the amount of a certain item by one 
     function increaseQuantity(id: string) {
-        const currentItems: CompleteItem[] = completeItem;
+        console.log("Trying to increase the amount of items")
+        const currentItems: CompleteItem[] = [...completeItems];
         for (let i = 0; i < currentItems.length; i++) {
-            if (completeItem[i].item.id === id) {
+            if (completeItems[i].item.id === id) {
                 currentItems[i].item.quantity++;
 
             }
 
         }
         setCompleteItems(currentItems)
-        calculateTotalPrice()
-
 
     }
     //Change the gift wrap boolean 
-    function chageGiftWrap(id: string) {
-        const currentItems: CompleteItem[] = completeItem;
+    function changeGiftWrap(id: string) {
+        const currentItems: CompleteItem[] = [...completeItems];
         for (let i = 0; i < currentItems.length; i++) {
-            if (completeItem[i].item.id === id) {
-                if (completeItem[i].item.giftWrap === true) {
-                    completeItem[i].item.giftWrap = false;
+            if (completeItems[i].item.id === id) {
+                if (completeItems[i].item.giftWrap === true) {
+                    completeItems[i].item.giftWrap = false;
                 }
                 else {
-                    completeItem[i].item.giftWrap = true
+                    completeItems[i].item.giftWrap = true
                 }
 
             }
 
         }
         setCompleteItems(currentItems)
-        calculateTotalPrice()
+        
     }
+    function calculateTotalPrice(completeItem:CompleteItem[]) {
+        let price: number = 0;
+        for (let i = 0; i < completeItem.length; i++) {
+            price = price + completeItem[i].itemInfo.price * completeItem[i].item.quantity;
+        }
+        return price;
+    }
+    const price :number = calculateTotalPrice(completeItems)
 
     // Returning the component ##STYLE HERE##
     return (
         <>
             <div className="container">
                 <p> Welcom to the page </p>
-                <button onClick={() => {
-                    fecthCompleteItems().then(() => calculateTotalPrice())
-                }
-                }>Show cart</button>
+
                 <ul>
                     {itemsToDisplay}
                 </ul>
-                <p> Total price: {TotalPrice}</p>
+                <p> Price is {price}</p>
 
 
             </div>
 
         </>
     )
-
-
-
-
-
-
-
-
-
 
 }
 //TSX interfaces used for this component
