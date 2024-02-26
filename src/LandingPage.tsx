6be50aa8b82a742ea9cd8263bf2a9d2249aad0a1
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2024-02-26 15:15:58 +0100

    Home page has all items as well as add to cart button

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,9 +1,28 @@
+import products from './Data/product.json';
+import { useState } from 'react';
 
 function LandingPage() {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+        console.log(cart);
+    }
     return (
         <div>
             <h1>This is a landing page</h1>
             <p>The very first page the customer sees, and where they'll be able to pick the items they want to buy</p>
+            <div>
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>
+                            <h2>{product.name}</h2>
+                            <p>{product.description}</p>
+                            <button onClick={() => addToCart(product)}>Add to cart</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
