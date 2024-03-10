f44a183a4236fc7f7adfdc08c6117944dfcd6691
Anton <boblo.ludvigsen@gmail.com>
2024-03-10 18:08:45 +0100

    Added the column with price, discount, total, shipping address (needs forms), billing address (needs button and form) and checkout button (does nothing)

--- src/Components/ShoppingCart.tsx
+++ src/Components/ShoppingCart.tsx
@@ -13,6 +13,32 @@ interface MyShoppinCartProps {
 
 
 function ShoppingCart(props: MyShoppinCartProps) {
+
+    function CheckoutSummary() {
+        const originalPrice = props.basket.totalPrice.priceBeforeRebate;
+        const total = props.basket.totalPrice.priceAfterRebate;
+        const discount = originalPrice - total;
+        
+        return (
+            <div className="checkoutContainer">
+                <div className="summaryBox">
+                    <h2>Original Price: </h2>
+                    <p>{originalPrice}</p>
+                    <h2>Discount:</h2>
+                    <p>{discount}</p>
+                    <h2>Total:</h2>
+                    <p>{total}</p>
+                </div>
+                <div className="addressBox">
+                    <h2>Shipping Address:</h2>
+                    {/* Forms go here */}
+                    <h2>Billing Address:</h2>
+                    {/* Checkbox for "same as shipping" */}
+                </div>
+                <button type="button" className="checkoutButton">Continue to checkout</button>
+            </div>
+        )
+    }
     function DisplayItemsInBasket() {
         if (props.basket.basketItems.length === 0) {
             return (
@@ -71,15 +97,14 @@ function ShoppingCart(props: MyShoppinCartProps) {
     }
     // Returning the component ##STYLE HERE##
     return (<>
-        <div className="cartItemsContainer">
-            <p> Shopping Basket  </p>
-
-
-            <DisplayItemsInBasket />
-
-            <p> Price before rebate is: {props.basket.totalPrice.priceBeforeRebate}</p>
-            <p> Your price after rebate is: {props.basket.totalPrice.priceAfterRebate} </p>
-
+        <div className="shoppingCartContainer">
+            <div className="cartItemsContainer">
+                <p>Shopping Basket</p>
+                <DisplayItemsInBasket />
+                <p> Price before rebate is: {props.basket.totalPrice.priceBeforeRebate}</p>
+                <p> Your price after rebate is: {props.basket.totalPrice.priceAfterRebate} </p>
+            </div>
+            <CheckoutSummary />
         </div>
     </>
     )
