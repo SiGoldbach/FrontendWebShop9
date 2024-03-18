dbbf5b0328d0387b2955af3bdbf8e734581d2c88
NicolaiUdbye <bj.polering@gmail.com>
2024-03-18 15:31:04 +0100

    Payment box and styling for forms page mostly done
    needs checkboxes and better styling for input fields

--- src/Components/forms.tsx
+++ src/Components/forms.tsx
@@ -48,58 +48,95 @@ function Forms() {
         }
     };
 
-    return <form action="/my-handling-form-page" method="post">
-        <ul>
-            <li>
-                <label htmlFor="firstName">First name:</label>
-                <input type="text" id="firstName" name="user_first_name" required/>
-            </li>
-            <li>
-                <label htmlFor="lastName">Last name:</label>
-                <input type="text" id="lastName" name="user_last_name" required/>
-            </li>
-            <li>
-                <label htmlFor="mail">Email:</label>
-                <input type="email" id="mail" name="user_email" required/>
-            </li>
-            <li>
-                <label htmlFor="address1">Address line 1:</label>
-                <input type="text" id="address1" name="user_address_1" required/>
-            </li>
-            <li>
-                <label htmlFor="address2">Address line 2:</label>
-                <input type="text" id="address2" name="user_address_2"/>
-            </li>
-            <li>
-                <label htmlFor="country">Country:</label>
-                <select id="country" name="user_country" required>
-                    <option value="denmark">Danmark</option>
-                </select>
-            </li>
-            <li>
-                <label htmlFor="zip">Zip code:</label>
-                <input type="number" id="zip" name="user_zip" required
-                       onChange={handleZipChange}
-                />
-            </li>
-            <li>
-                <label htmlFor="city">City:</label>
-                <input type="text" id="city" name="user_city" required/>
-            </li>
-            <li>
-                <label htmlFor="phone">Phone number:</label>
-                <input type="text" id="phone" name="user_phone"/>
-            </li>
-            <li>
-                <label htmlFor="company">Company:</label>
-                <input type="text" id="company" name="company_name"/>
-            </li>
-            <li>
-                <label htmlFor="companyVAT">Company VAT:</label>
-                <input type="text" id="companyVAT" name="company_VAT"/>
-            </li>
-        </ul>
-    </form>
+    return (<>
+        <div className='shoppingCartContainer'>
+            <div className="formsContainer">
+                <form action="/my-handling-form-page" method="post">
+                    <ul>
+                        <li>
+                            <label htmlFor="firstName">First name:</label>
+                            <input type="text" id="firstName" name="user_first_name" required/>
+                        </li>
+                        <li>
+                            <label htmlFor="lastName">Last name:</label>
+                            <input type="text" id="lastName" name="user_last_name" required/>
+                        </li>
+                         <li>
+                            <label htmlFor="mail">Email:</label>
+                            <input type="email" id="mail" name="user_email" required/>
+                        </li>
+                        <li>
+                            <label htmlFor="address1">Address line 1:</label>
+                            <input type="text" id="address1" name="user_address_1" required/>
+                        </li>
+                        <li>
+                            <label htmlFor="address2">Address line 2:</label>
+                            <input type="text" id="address2" name="user_address_2"/>
+                        </li>
+                        <li>
+                            <label htmlFor="country">Country:</label>
+                            <select id="country" name="user_country" required>
+                                <option value="denmark">Danmark</option>
+                            </select>
+                        </li>
+                        <li>
+                            <label htmlFor="zip">Zip code:</label>
+                            <input type="number" id="zip" name="user_zip" required
+                                onChange={handleZipChange}
+                            />
+                        </li>
+                        <li>
+                            <label htmlFor="city">City:</label>
+                            <input type="text" id="city" name="user_city" required/>
+                        </li>
+                        <li>
+                            <label htmlFor="phone">Phone number:</label>
+                            <input type="text" id="phone" name="user_phone"/>
+                        </li>
+                        <li>
+                            <label htmlFor="company">Company:</label>
+                            <input type="text" id="company" name="company_name"/>
+                        </li>
+                        <li>
+                            <label htmlFor="companyVAT">Company VAT:</label>
+                            <input type="text" id="companyVAT" name="company_VAT"/>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <div className="paymentContainer">
+                <div>
+                    <form action="/my-handling-payment-page" method="post">
+                        <ul>
+                            <li>
+                                <label htmlFor="kortnummer">Kortnummer:</label>
+                                <input type="text" id="kortnummer" name="kort_nummer"/>
+                            </li>
+                            <li>
+                                <label htmlFor="udloebsdato">MM/YY:</label>
+                                <input type="text" id="udloebsdato" name="kort_udloebsdato"/>
+                            </li>
+                            <li>
+                                <label htmlFor="sikkerhedskode">Sikkerhedskode*:</label>
+                                <input type="text" id="sikkerhedskode" name="kort_sikkerhedskode"/>
+                            </li>
+                        </ul>
+                    </form>
+                    <div className="checkboxBox">
+                        <div className="CheckBox">
+                            <p>ToDo CheckBox for marketing emails</p>
+                        </div>
+                        <div className="CheckBox">
+                            <p>ToDo CheckBox for the terms & conditions</p>
+                        </div>
+                        <div className="line"></div>                
+                    </div>
+                    <button type="button" className="orderButton"> Place Order </button>
+                </div>
+            </div>
+        </div>
+    </>
+    )
 }
 
 
