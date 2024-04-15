7e0b5f09576d7faf407c8ab3ccb65b45f5220176
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-15 15:07:45 +0200

    I have removed unused files and updated fetch for get items

--- src/Networking/networking.ts
+++ src/Networking/networking.ts
@@ -1,25 +1,12 @@
 import { ProductInfo, Basket,CustomerInfo,OrderInformation } from "../TSReusedTypes/ItemsAndPrices.js"
 
 
-//This function is getting all the items that needs to be displayed in the store 
+//This function is getting all the items that needs to be displayed in the store. 
 export async function getItems(): Promise<ProductInfo[]>{
-    let data: ProductInfo[] =[];
-
     const URL="https://raw.githubusercontent.com/larsthorup/checkout-data/main/product-v2.json"
     const response = await fetch(URL);
-    const responseAsJson= await response.json();
-    data = responseAsJson.map(({ id, name,price,currency,rebateQuantity,rebatePercent,upsellProductId,imageUrl }:
-        {id: string, name: string,price: number, currency: string,rebateQuantity:number,rebatePercent:number,upsellProductId:string,imageUrl:string}) => {
-        return { id: id,
-                 name: name,
-                 price: price, 
-                 currency: currency, 
-                 rebateQuantity: rebateQuantity, 
-                 rebatePercent: rebatePercent, 
-                 upsellProductId: upsellProductId, 
-                 imageUrl: imageUrl };
-    });
-    return data
+    const responseAsJson:ProductInfo[] = await response.json();
+    return responseAsJson;
 }
 //This is the function for submitting an order. 
 //Its very poorly implemented right now needs to change. 
