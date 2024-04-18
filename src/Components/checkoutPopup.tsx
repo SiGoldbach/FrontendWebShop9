2d71a1f527b8e0866408f4a08148ffa746af6641
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-04-18 12:08:26 +0200

    quick html name changes for easier CSS management

--- src/Components/checkoutPopup.tsx
+++ src/Components/checkoutPopup.tsx
@@ -10,6 +10,7 @@ type popuptForUpsellProductprops={
 
 
 export function CheckoutPopUp(props:popuptForUpsellProductprops){
+    console.log("Checkout Popup was here!")
     const basketDispatchercontext= useBasketDispatchContext();
 
     function handleAndClearBasket(){
@@ -19,21 +20,19 @@ export function CheckoutPopUp(props:popuptForUpsellProductprops){
     
 
     return(
-        <div className="form-popup">
-            <div>
-                <p> Successful transaction </p>
-                <img className="product-image"
-                    //src={upsellProduct.imageUrl}
-                    alt="PlaceholderImage" />
-                <p> Order Successful </p>
+      <div className="checkout-popup">
+        <div className="text_and_image">
+          <p> Successful transaction </p>
+          <img className="product-image"
+            //src={upsellProduct.imageUrl}
+            alt="PlaceholderImage" />
+          <p> Order Successful </p>
 
-                <div className="form-button-div">
-                    <div className="form-button-div">
-                        <button className="add-to-cart-button" onClick={handleAndClearBasket}> Continue</button>
-                    </div>
-                </div>
-            </div>
+          <div className="continue_button">
+            <button className="add-to-cart-button" onClick={handleAndClearBasket}> Continue</button>
+          </div>
         </div>
+      </div>
     )
 }
 
