facd1b189821c5c8a759b97423a481f30fe691ee
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-04-18 12:14:48 +0200

    exit button for top right corner and indents fixed to 2 spaces

--- src/Components/checkoutPopup.tsx
+++ src/Components/checkoutPopup.tsx
@@ -5,23 +5,24 @@ import "../Pages/index.css"
 
 
 type popuptForUpsellProductprops={
-    closePopUp: ()=>void;
+  closePopUp: ()=>void;
 } 
 
 
 export function CheckoutPopUp(props:popuptForUpsellProductprops){
-    console.log("Checkout Popup was here!")
+  console.log("Checkout Popup was here!")
     const basketDispatchercontext= useBasketDispatchContext();
 
     function handleAndClearBasket(){
-        basketDispatchercontext({type: BasketItemKind.ClEARBASKET});
-        props.closePopUp
+      basketDispatchercontext({type: BasketItemKind.ClEARBASKET});
+      props.closePopUp
     }
     
 
     return(
       <div className="checkout-popup">
         <div className="text_and_image">
+          <button className="exit-button" onClick={props.closePopUp}> X</button>
           <p> Successful transaction </p>
           <img className="product-image"
             //src={upsellProduct.imageUrl}
