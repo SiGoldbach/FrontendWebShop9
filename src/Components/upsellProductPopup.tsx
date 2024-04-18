bc8c3aa528fc153bec462d421c078263dc7c84a5
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-04-18 13:36:41 +0200

    indent changes and all errors in the different files fixed

--- src/Components/upsellProductPopup.tsx
+++ src/Components/upsellProductPopup.tsx
@@ -8,43 +8,44 @@ import {ImageHandler} from"./ImageHanlder"
 
 
 type popuptForUpsellProductprops={
-    closePopUp: ()=>void;
-    currentid: string;
-    upsellId: string | null;
-
+  closePopUp: ()=>void;
+  currentid: string;
+  upsellId: string | null;
 } 
 
 
 export function PopUpForUpsellProduct({closePopUp,currentid,upsellId}:popuptForUpsellProductprops){
-    console.log(currentid);
-    console.log(upsellId);
-    const products = useProductContext();
-    const basketDispatchercontext= useBasketDispatchContext();
-    const upsellProduct:ProductInfo = products.filter((product)=>product.product_id==upsellId)[0]
+  console.log(currentid);
+  console.log(upsellId);
+  const products = useProductContext();
+  const basketDispatchercontext= useBasketDispatchContext();
+  const upsellProduct:ProductInfo = products.filter((product)=>product.product_id==upsellId)[0]
 
-    function handleUpgrade(){
-        basketDispatchercontext({type: BasketItemKind.REPLACEITEMINBASKET,currentItemId: currentid,newProduct: upsellProduct});
-        closePopUp
-    }
+  function handleUpgrade(){
+    basketDispatchercontext({type: BasketItemKind.REPLACEITEMINBASKET,currentItemId: currentid,newProduct: upsellProduct});
+    closePopUp
+  }
     
 
 
-    return(<div className="form-popup">
-        <div>
+  return(
+    <div className="form-popup">
+      <div>
         <p>{upsellProduct.name} </p>
         <ImageHandler url={upsellProduct.image_url}/>
         <p> Do you want to upgrade? </p>
         <p> Price: {upsellProduct.price} {upsellProduct.currency}</p>
 
         <div className="form-button-div">
-            <div className="form-button-div">
-                <button className="add-to-cart-button" onClick={closePopUp}> Cancel</button>
-            </div>
-            <div className="form-button-div">
-                <button className="add-to-cart-button" onClick={handleUpgrade}> Upgrade</button>
-            </div>
+          <div className="form-button-div">
+            <button className="add-to-cart-button" onClick={closePopUp}> Cancel</button>
+          </div>
+          <div className="form-button-div">
+            <button className="add-to-cart-button" onClick={handleUpgrade}> Upgrade</button>
+          </div>
         </div>
-        </div>
-        </div>)
+      </div>
+    </div>
+  )
 }
 
--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -10,19 +10,19 @@ function LandingPage() {
 
 
     return (
-        <div className="landing-page-container">
-            <h1>This is a landing page</h1>
-            <p>The very first page the customer sees, and where they'll be able to pick the items they want to buy</p>
-            <div className="products-container">
-                {products.map((product) => (
-                    <div className="product-card" key={product.id}>
-                        <h2 className="product-name">{product.name}</h2>
-                        <p className="product-description">{product.price} {product.currency}</p>
-                        <button className="add-to-cart-button" onClick={() => basketDispatcher({type: BasketItemKind.ADDTOBASKET,productinfo:product})}>Add to cart</button>
-                    </div>
-                ))}
+      <div className="landing-page-container">
+        <h1>This is a landing page</h1>
+        <p>The very first page the customer sees, and where they'll be able to pick the items they want to buy</p>
+        <div className="products-container">
+          {products.map((product) => (
+            <div className="product-card" key={product.product_id}>
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-description">{product.price} {product.currency}</p>
+              <button className="add-to-cart-button" onClick={() => basketDispatcher({type: BasketItemKind.ADDTOBASKET,productinfo:product})}>Add to cart</button>
             </div>
+          ))}
         </div>
+      </div>
     )
 }
 
