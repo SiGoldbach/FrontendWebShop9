d6f51197bcf4c86065eccce1c81218e9d501d169
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-25 16:10:13 +0200

    Added try cath in get and set sessionstorage for the basket

--- src/State/SessionStorage.ts
+++ src/State/SessionStorage.ts
@@ -4,22 +4,37 @@ import { createNewEmptyBasket } from "./BasketState";
 const basketKey: string="basket";
 //This function will save 
 export function storeBasketInSession(basket: Basket){
-    console.log("I am storing the basket");
-    sessionStorage.setItem(basketKey,JSON.stringify(basket));
+    try{
+        console.log("I am storing the basket");
+        sessionStorage.setItem(basketKey,JSON.stringify(basket));
+    
+    }catch(err){
+        console.log("It seems like your browser does not support session storage trying to save basket ");
+
+
+    }
+    
 }
 //The basket should never be null since it is instantiated when rendering app which is akways rendered
 //Since it is the parent of the components this should be used ind. 
 export function getBasketInSession(): Basket|null{
+    //This needs to be surounded by try catch to avoid app crashes due to browsers that are not willing to use session storage
+    try{
+        const basketAsString = sessionStorage.getItem(basketKey)
+        console.log(basketAsString);
+        if(basketAsString!=null){
+            const basket: Basket = JSON.parse(basketAsString);
+            return basket;
+    
+        }
+        else return null;
+    }catch(err){
+        console.log("It seems like your browser does not support session storage trying to get basket ");
+        return null;
+    }
     
 
-    const basketAsString = sessionStorage.getItem(basketKey)
-    console.log(basketAsString);
-    if(basketAsString!=null){
-        const basket: Basket = JSON.parse(basketAsString);
-        return basket;
-
-    }
-    else return null;
+    
 }
 //Function that either returns an empty new basket or the stored session basket
 export function getBasket():Basket{
